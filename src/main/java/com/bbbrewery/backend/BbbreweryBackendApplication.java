ae385d943e118ae950a9ff59198ea9821dc80644
Jaulin <n.jaulin2016@gmail.com>
2025-07-23 06:04:05 -0400

    First commit - Java Spring Boot backend & Oracle SQL Dev

---
+++ src/main/java/com/bbbrewery/backend/BbbreweryBackendApplication.java
@@ -0,0 +1,13 @@
+package com.bbbrewery.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BbbreweryBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BbbreweryBackendApplication.class, args);
+	}
+
+}
